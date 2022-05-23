9241ddbf8f553cf9f0a9af57c3bc8311987253e8
phlima3 <psantoslima86@gmail.com>
2022-05-23 17:25:01 -0300

    firebase integrated into the project and set environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './Services/firebase';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
