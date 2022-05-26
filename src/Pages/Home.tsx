53c35508e65342364d6685497294e2b6b39ae240
phlima3 <psantoslima86@gmail.com>
2022-05-26 17:36:21 -0300

    Created join room flow

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,9 @@
+import { FormEvent, useState } from "react";
 import illustrationImg from "../Assets/Images/illustration.svg";
 import logoImage from "../Assets/Images/logo.svg";
 import googleImage from "../Assets/Images/google-icon.svg";
 import loginImage from "../Assets/Images/log-in.svg";
+import { database } from "../Services/firebase";
 import { Button } from "../Components/Button";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../Hooks/useAuth";
@@ -10,12 +12,29 @@ import "../Styles/auth.scss";
 export function Home() {
   const navigate = useNavigate();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
     }
     navigate("rooms");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exist");
+      return;
+    }
+    navigate(`rooms/${roomCode}`);
   }
 
   return (
@@ -35,8 +54,13 @@ export function Home() {
 
           <div className="separator">ou entre em uma sala</div>
 
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">
               <img src={loginImage} alt="Logo do Google" />
               Entrar na sala
