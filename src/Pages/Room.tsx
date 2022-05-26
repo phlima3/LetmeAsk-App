f08e3ffc79ae1f56681c32b7ae3bac9db10a86ed
phlima3 <psantoslima86@gmail.com>
2022-05-26 17:55:15 -0300

    Start development Room scrren, but no finished

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -1,3 +1,30 @@
+import logoImage from "../Assets/Images/logo.svg";
+import { Button } from "../Components/Button";
+
 export function Room() {
-  return <h1>Sala</h1>;
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImage} alt="letmeask" />
+          <div>codigo</div>
+        </div>
+      </header>
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 Perguntas</span>
+        </div>
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
