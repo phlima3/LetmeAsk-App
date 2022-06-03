21d8a9844023bff0fed27c8f73f8cb0272bc191f
phlima3 <psantoslima86@gmail.com>
2022-06-03 17:19:36 -0300

    Add Question to Firebase and style Room Question Page

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -1,15 +1,67 @@
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import logoImage from "../Assets/Images/logo.svg";
 import { Button } from "../Components/Button";
+import { RoomCode } from "../Components/RoomCode";
+import { useAuth } from "../Hooks/useAuth";
 
 import "../Styles/room.scss";
+import "react-toastify/dist/ReactToastify.css";
+import { database } from "../Services/firebase";
+
+type RoomParams = {
+  id: string;
+};
 
 export function Room() {
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState("");
+
+  const roomID = params.id!;
+
+  async function handleSendNewQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === "") {
+      return;
+    }
+    if (!user) {
+      toast.error(
+        "É necessário que você esteja logado para fazer uma pergunta",
+        {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        }
+      );
+      return console.log(Error);
+    }
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomID}/questions`).push(question);
+
+    setNewQuestion("");
+  }
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImage} alt="letmeask" />
-          <div>codigo</div>
+          <RoomCode code={roomID} />
         </div>
       </header>
       <main>
@@ -17,16 +69,30 @@ export function Room() {
           <h1>Sala React</h1>
           <span>4 Perguntas</span>
         </div>
-        <form>
-          <textarea placeholder="O que você quer perguntar?" />
+        <form onSubmit={handleSendNewQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <div className="form-footer">
-            <span>
-              Para enviar uma pergunta, <button>faça seu login</button>
-            </span>
-            <Button type="submit">Enviar pergunta</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt="Avatar do usuário" />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça seu login</button>
+              </span>
+            )}
+            <Button type="submit" disabled={!user}>
+              Enviar pergunta
+            </Button>
           </div>
         </form>
       </main>
+      <ToastContainer />
     </div>
   );
 }
